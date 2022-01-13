d573780f268fb9b24eb62db2739a15e7f8c9006e
Kamola77777 <kamola_8880@mail.ru>
2022-01-13 23:12:05 +0500

    add: userDetails

--- src/components/ImportantDetails/ImporDetails.jsx
+++ src/components/ImportantDetails/ImporDetails.jsx
@@ -5,27 +5,29 @@ import { useParams } from "react-router-dom";
 
 export default function ImporDetails(props) {
   console.log(props);
-  const [users, setUsers] = useState({});
-  const params = useParams();
-  console.log(params);
+  const [user, setUser] = useState({});
+  const {userId} = useParams();
+  console.log(userId);
 
-  const fetchUsersById = (userId) => {
+  const fetchUserById = (userId) => {
     fetch("https://jsonplaceholder.typicode.com/users/" + userId)
       .then((res) => res.json())
-      .then((data) => setUsers(data))
+      .then((data) => setUser(data))
       .catch((err) => {
         console.error(err);
       });
   };
   useEffect(() => {
-    fetchUsersById();
-  }, []);
-  console.log(users);
+    fetchUserById(userId);
+  }, [userId]);
+  
+
   return (
     <div className="h5">
     
-      <h4>{users.name}</h4>
-      <h4>{users.email}</h4>
+      <h4>{user.name}</h4>
+      <h4>{user.email}</h4>
+      <h4>{user.company?.name}</h4>
     </div>
   );
 }
